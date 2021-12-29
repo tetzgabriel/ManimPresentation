580c09c4a33320f8fecec3e663175e93369a6bec
Gabriel Tetzlaf Mansano <tetz.gabriel@gmail.com>
2021-12-29 00:04:38 -0300

    Feature | Created main title and added .gitignore

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from manim import *
-class DefaultTemplate(Scene):
+class MainTitleScene(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+        dir_str = [ 'UP', 'DOWN', 'LEFT', 'RIGHT']
+        dir_list = [ UP, DOWN, LEFT, RIGHT]
 
-        square = Square()  # create a square
-        square.flip(RIGHT)  # flip horizontally
-        square.rotate(-3 * TAU / 8)  # rotate a certain amount
-
-        self.play(Create(square))  # animate the creation of the square
-        self.play(Transform(square, circle))  # interpolate the square into the circle
-        self.play(FadeOut(square))  # fade out animation
+        courseTitle = Text('Comunicação técnica e científica', font_size = 30).shift(RIGHT * 3, DOWN * 1.5)
+        mainTitle = Text('Manim: da história até agora', font_size = 60).next_to(courseTitle, DOWN).shift(LEFT * 2)
+        
+        self.play(Write(courseTitle))
+        self.play(Write(mainTitle))
