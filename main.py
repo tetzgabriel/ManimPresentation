dead1756a9d413d1dbf8ea63ec8f3ac67a4a74f8
Gabriel Tetzlaf Mansano <tetz.gabriel@gmail.com>
2022-01-04 23:33:15 -0300

    Feature | Added installation scenes && Added crazy examples scenes && Added Community scenes

--- main.py
+++ main.py
@@ -139,6 +139,107 @@ class ManimDiferentials3(Scene):
         self.add(manimTitle)
         self.play(Write(logo))
 
+class Community(Scene):
+    def construct(self):
+        manimTitle = Text('Atenção da comunidade', font_size = 30).shift(UP * 3)
+        
+        manimDefinition = Text('Manim atraiu desenvolvedores e produtores de vídeo', font_size = 25).next_to(manimTitle, DOWN).shift(LEFT * 2.5, DOWN)
+        manimCreation = Text('Surge necessidade de novas funções', font_size = 25).next_to(manimDefinition, DOWN).shift(LEFT * 1.2, DOWN * 0.5)
+        manimObjective = Text('Em 2020 surge a versão Community', font_size = 25).next_to(manimCreation, DOWN).shift(RIGHT * 0.05, DOWN * 0.5)
+        
+        self.play(Write(manimTitle))
+        self.play(Write(manimDefinition))
+        self.play(Write(manimCreation))
+        self.play(Write(manimObjective))
+
+class Installation(Scene):
+    def construct(self):
+        manimTitle = Text('Como utilizar o Manim', font_size = 30).shift(UP * 3)
+        
+        manimDefinition = Text('· Instalar Python', font_size = 25).next_to(manimTitle, DOWN).shift(LEFT * 2.5, DOWN)
+        manimCreation = Text('· Instalar LaTeX', font_size = 25).next_to(manimDefinition, DOWN).shift(DOWN * 0.5)
+        manimObjective = Text('· Instalar Manim utilizando pip', font_size = 25).next_to(manimCreation, DOWN).shift(RIGHT, DOWN * 0.5)
+        
+        self.play(Write(manimTitle))
+        self.play(Write(manimDefinition))
+        self.play(Write(manimCreation))
+        self.play(Write(manimObjective))
+
+class BooleanOperations(Scene):
+    def construct(self):
+        ellipse1 = Ellipse(
+            width=4.0, height=5.0, fill_opacity=0.5, color=BLUE, stroke_width=10
+        ).move_to(LEFT)
+        ellipse2 = ellipse1.copy().set_color(color=RED).move_to(RIGHT)
+        bool_ops_text = Text('Rompendo barreiras com o Manim', font_size = 30).next_to(ellipse1, UP * 3)
+        ellipse_group = Group(bool_ops_text, ellipse1, ellipse2).move_to(LEFT * 3)
+        self.play(FadeIn(ellipse_group))
+
+        i = Intersection(ellipse1, ellipse2, color=GREEN, fill_opacity=0.5)
+        self.play(i.animate.scale(0.25).move_to(RIGHT * 5 + UP * 2.5))
+        intersection_text = Text("Intersection", font_size=23).next_to(i, UP)
+        self.play(FadeIn(intersection_text))
+
+        u = Union(ellipse1, ellipse2, color=ORANGE, fill_opacity=0.5)
+        union_text = Text("Union", font_size=23)
+        self.play(u.animate.scale(0.3).next_to(i, DOWN, buff=union_text.height * 3))
+        union_text.next_to(u, UP)
+        self.play(FadeIn(union_text))
+
+        e = Exclusion(ellipse1, ellipse2, color=YELLOW, fill_opacity=0.5)
+        exclusion_text = Text("Exclusion", font_size=23)
+        self.play(e.animate.scale(0.3).next_to(u, DOWN, buff=exclusion_text.height * 3.5))
+        exclusion_text.next_to(e, UP)
+        self.play(FadeIn(exclusion_text))
+
+        d = Difference(ellipse1, ellipse2, color=PINK, fill_opacity=0.5)
+        difference_text = Text("Difference", font_size=23)
+        self.play(d.animate.scale(0.3).next_to(u, LEFT, buff=difference_text.height * 3.5))
+        difference_text.next_to(d, UP)
+        self.play(FadeIn(difference_text))
+
+class FollowingGraphCamera(MovingCameraScene):
+    def construct(self):
+        manimTitle = Text('Rompendo barreiras com o Manim', font_size = 30).shift(UP * 3)
+        self.play(Write(manimTitle))
+        self.camera.frame.save_state()
+
+        # create the axes and the curve
+        ax = Axes(x_range=[-1, 10], y_range=[-1, 10])
+        graph = ax.plot(lambda x: np.sin(x), color=BLUE, x_range=[0, 3 * PI])
+
+        # create dots based on the graph
+        moving_dot = Dot(ax.i2gp(graph.t_min, graph), color=ORANGE)
+        dot_1 = Dot(ax.i2gp(graph.t_min, graph))
+        dot_2 = Dot(ax.i2gp(graph.t_max, graph))
+
+        self.add(ax, graph, dot_1, dot_2, moving_dot)
+        self.play(self.camera.frame.animate.scale(0.5).move_to(moving_dot))
+
+        def update_curve(mob):
+            mob.move_to(moving_dot.get_center())
+
+        self.camera.frame.add_updater(update_curve)
+        self.play(MoveAlongPath(moving_dot, graph, rate_func=linear))
+        self.camera.frame.remove_updater(update_curve)
+
+        self.play(Restore(self.camera.frame))
+
+class ThreeDCameraIllusionRotation(ThreeDScene):
+    def construct(self):
+        manimTitle = Text('Rompendo barreiras com o Manim', font_size = 30).shift(UP * 6.5)
+
+        axes = ThreeDAxes()
+        circle=Circle()
+        circle2=Circle().scale(2)
+        self.set_camera_orientation(phi = 10 * DEGREES, theta = 100 * DEGREES)
+        self.add(circle, circle2, axes)
+        self.begin_3dillusion_camera_rotation(rate = 2)
+        self.play(Write(manimTitle))
+        self.wait(PI)
+        self.stop_3dillusion_camera_rotation()
+
+
 class Thanks(Scene):
     def construct(self):
         thanks = Text('Obrigado!', font_size = 30)
